40476460db258bb6cf162d8d7b5b68a3c1d4e343
AL1ATE <102225423+AL1ATE@users.noreply.github.com>
2022-05-06 11:26:44 +0300

    Create SimpleQ.java

---
+++ Task 3 (14)/src/ru/vsu/cs/obukhov/SimpleQ.java
@@ -0,0 +1,103 @@
+package ru.vsu.cs.obukhov;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Queue;
+
+public class SimpleQ<T> implements Queue<T> {
+
+    private ArrayList<T> list = new ArrayList<>();
+
+    @Override
+    public boolean add(T item){
+
+        return list.add(item);
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean offer(T t) {
+        return false;
+    }
+
+    @Override
+    public T remove(){
+        return list.remove(0);
+    }
+
+    @Override
+    public T poll() {
+        return null;
+    }
+
+    @Override
+    public T element() {
+        return null;
+    }
+
+    @Override
+    public T peek() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty(){
+        return list.isEmpty();
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public <T1> T1[] toArray(T1[] a) {
+        return null;
+    }
+
+}
